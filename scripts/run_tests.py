ceba88df4945bdf15492154187ec5eecbfb9afbf
diogo464 <djfona@gmail.com>
2022-12-28 18:25:58 +0000

    update

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -38,13 +38,7 @@ def read_test_cases() -> list[TestCase]:
 def run_test_case(kind: RunKind, test_case: TestCase):
     subcmd = "run" if kind == RunKind.INTERPRETED else "crun"
     outputproc = subprocess.run(
-        [
-            "mvn",
-            "-q",
-            "exec:java",
-            "-Dexec.mainClass=App",
-            f"-Dexec.args={subcmd} {test_case.name}",
-        ],
+        "mvn -q exec:java -Dexec.mainClass=App -Dexec.args=\"{subcmd} {test_case.name}\"",
         shell=True,
         capture_output=True,
         env={"MAVEN_OPTS": "-ea"},
